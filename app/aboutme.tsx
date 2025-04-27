b1f4a738a313585cdb8cb8a516c94eadd5f5e693
Andy Méthion <andymethion@gmail.com>
2025-04-27 21:42:34 +0200

    feat: About Me section

---
+++ app/aboutme.tsx
@@ -0,0 +1,13 @@
+export default function AboutMe() {
+  return (
+    <section className="my-16 flex justify-center" id="aboutme">
+      <div className="flex w-11/12 flex-col gap-8">
+        <p className="text-4xl font-bold text-center">About Me</p>
+        <p className="text-center text-balance">Hi! I'm Andy. A young passionate developer who started out creating websites in junior high school</p>
+        <p className="text-center text-balance">Over the years, my focus has shifted to backend development, and in particular to the production of automation programs</p>
+        <p className="text-center text-balance">I discovered blockchain by selling these kinds of scripts to individuals A futuristic technology that will revolutionize our perception of money, transparency and ownership</p>
+        <p className="text-center text-balance">Blockchain is a subject I'm obsessed with, which naturally led me to learn how to develop programs on it</p>
+      </div>
+    </section>
+  );
+}
