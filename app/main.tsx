a9946d6cbbd0eb6642d9f36f3e45d30dc6a00473
Andy Méthion <andymethion@gmail.com>
2025-04-29 21:55:10 +0200

    fix: convert text to javascript expression to avoid escaped characters

--- app/main.tsx
+++ app/main.tsx
@@ -8,7 +8,7 @@ export default function Main() {
     <main className="flex justify-center pt-16 pb-8">
       <div className="flex w-11/12 flex-col items-center gap-8 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-6/12 2xl:w-5/12">
         <h1 className="text-center text-4xl font-bold text-balance">Je transforme mes idées en programmes fonctionnels et évolutifs.</h1>
-        <p className="text-center text-balance">Jeune développeur passionné par les programmes d'automatisation et le développement blockchain. Do you live and breathe code?</p>
+        <p className="text-center text-balance">{"Jeune développeur passionné par les programmes d'automatisation et le développement blockchain. Do you live and breathe code?"}</p>
         <a href="mailto:andymethion@gmail.com">
           <Button onClick={() => toast("Email copié dans le presse papier")} className="text-muted-foreground flex w-fit cursor-pointer items-center gap-2" variant="secondary">
             <svg className="h-5" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
