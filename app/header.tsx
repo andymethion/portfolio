01596902eacfb8aa279c5116208ccd2d584d8f97
Andy Méthion <andymethion@gmail.com>
2025-09-11 11:26:37 +0200

    refactor: Header code optimization

--- app/header.tsx
+++ app/header.tsx
@@ -1,77 +1,44 @@
-import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from "@/components/ui/navigation-menu";
-import { Drawer, DrawerContent, DrawerTrigger, DrawerClose } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerFooter, DrawerTrigger } from "@/components/ui/drawer";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+const sections = [
+  { name: "À propos de moi", href: "#aboutme" },
+  { name: "Mes projets", href: "#projects" },
+  { name: "Ma stack technique", href: "#techstack" },
+  { name: "Me contacter", href: "#contact" },
+];
 
 export default function Header() {
   return (
-    <header className="border-b-border flex h-16 justify-center border-b border-dashed">
-      <div className="flex w-11/12 items-center justify-between sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-6/12 2xl:w-5/12">
-        <p>{"Portfolio d'Andy"}</p>
-        <NavigationMenu className="hidden lg:flex">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink className="cursor-pointer" href="#aboutme">
-                À propos de moi
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className="cursor-pointer" href="#projects">
-                Mes projets
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className="cursor-pointer" href="#techstack">
-                Ma stack technique
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className="cursor-pointer" href="#contact">
-                Me contacter
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+    <header className="flex h-16 items-center justify-center">
+      <div className="flex w-11/12 justify-between sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-6/12 2xl:w-5/12">
+        <p>Portfolio d'Andy</p>
         <Drawer>
           <DrawerTrigger className="lg:hidden">
-            <svg className="h-5 cursor-pointer" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
-            </svg>
+            <Menu />
           </DrawerTrigger>
           <DrawerContent>
-            <div className="m-4 flex justify-center">
-              <div className="flex w-11/12 flex-col gap-2">
-                <DrawerClose asChild>
-                  <a href="#aboutme">
-                    <Button className="w-full cursor-pointer" variant="ghost">
-                      À propos de moi
-                    </Button>
-                  </a>
-                </DrawerClose>
-                <DrawerClose asChild>
-                  <a href="#projects">
-                    <Button className="w-full cursor-pointer" variant="ghost">
-                      Mes projects
-                    </Button>
-                  </a>
-                </DrawerClose>
-                <DrawerClose asChild>
-                  <a href="#techstack">
-                    <Button className="w-full cursor-pointer" variant="ghost">
-                      Ma stack technique
-                    </Button>
-                  </a>
-                </DrawerClose>
-                <DrawerClose asChild>
-                  <a href="#contact">
-                    <Button className="w-full cursor-pointer" variant="ghost">
-                      Me contacter
-                    </Button>
-                  </a>
-                </DrawerClose>
-              </div>
-            </div>
+            <DrawerFooter>
+              {sections.map((section) => (
+                <Button asChild key={section.name} variant="ghost">
+                  <Link href={section.href}>{section.name}</Link>
+                </Button>
+              ))}
+            </DrawerFooter>
           </DrawerContent>
         </Drawer>
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList>
+            {sections.map((section) => (
+              <NavigationMenuItem key={section.name}>
+                <NavigationMenuLink href={section.href}>{section.name}</NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
     </header>
   );
