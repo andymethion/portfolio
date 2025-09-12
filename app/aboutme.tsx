cb61ed3a3fef0a272d1df1717e139b4311ffde01
Andy Méthion <andymethion@gmail.com>
2025-09-12 20:47:53 +0200

    refactor: simplify AboutMe component by using a texts array for rendering paragraphs

--- app/aboutme.tsx
+++ app/aboutme.tsx
@@ -1,16 +1,22 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+const texts = [
+  "Bonjour ! Je m'appelle Andy, étudiant en informatique à l'IUT de Nantes. Je me considère comme un jeune développeur passionné ayant débuté dès le collège par la création de sites internet.",
+  "Avec le temps, mon intérêt s'est porté sur le développement backend, et plus particulièrement sur la réalisation de programmes automatisés capables de simuler des comportements humains sur des sites internet.",
+  "J'ai découvert la blockchain en vendant des programmes d'automatisation à des particuliers. Cette technologie futuriste est appelée à transformer profondément notre perception de l'argent, de la transparence et de la propriété.",
+  "La blockchain est un sujet qui me passionne au point d'y penser quotidiennement. C'est dans ce contexte que je me suis également engagé dans le développement blockchain. Il s'agit probablement de l'un des écosystèmes les plus solides à ce jour.",
+  "Mon projet professionnel consiste à adopter de bonnes habitudes de travail en entreprise et à perfectionner mes compétences dans des domaines que je considère comme essentiels. En parallèle, je souhaite continuer à développer mes projets personnels.",
+];
+
 export default function AboutMe() {
   return (
     <section className="flex justify-center py-8" id="aboutme">
       <div className="flex w-11/12 flex-col items-center gap-8 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-7/12">
         <p className="text-center text-4xl font-bold">{"À propos de moi"}</p>
-        <p className="text-center text-balance">{"Bonjour ! Je m'appelle Andy, étudiant en informatique à l'IUT de Nantes. Je me considère comme un jeune développeur passionné ayant débuté dès le collège par la création de sites internet."}</p>
-        <p className="text-center text-balance">{"Avec le temps, mon intérêt s'est porté sur le développement backend, et plus particulièrement sur la réalisation de programmes automatisés capables de simuler des comportements humains sur des sites internet."}</p>
-        <p className="text-center text-balance">{"J'ai découvert la blockchain en vendant des programmes d'automatisation à des particuliers. Cette technologie futuriste est appelée à transformer profondément notre perception de l'argent, de la transparence et de la propriété."}</p>
-        <p className="text-center text-balance">{"La blockchain est un sujet qui me passionne au point d'y penser quotidiennement. C'est dans ce contexte que je me suis également engagé dans le développement blockchain. Il s'agit probablement de l'un des écosystèmes les plus solides à ce jour."}</p>
-        <p className="text-center text-balance">{"Mon projet professionnel consiste à adopter de bonnes habitudes de travail en entreprise et à perfectionner mes compétences dans des domaines que je considère comme essentiels. En parallèle, je souhaite continuer à développer mes projets personnels."}</p>
+        {texts.map((text) => (
+          <p className="text-center text-balance">{text}</p>
+        ))}
         <Button asChild variant="secondary">
           <Link href="CV.pdf" target="_blank">
             Télécharger mon Curriculum Vitae
