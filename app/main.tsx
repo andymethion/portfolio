2312ee21da535f34998eb8ad39431cfceac24fda
Andy Méthion <andymethion@gmail.com>
2025-09-12 20:49:31 +0200

    refactor: improve code formatting for better readability in Main component

--- app/main.tsx
+++ app/main.tsx
@@ -15,8 +15,14 @@ export default function Main() {
   return (
     <main className="flex justify-center pt-16 pb-8">
       <div className="flex w-11/12 flex-col items-center gap-8 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12">
-        <h1 className="text-center text-4xl font-bold text-balance">{"Je transforme mes idées en programmes fonctionnels et évolutifs."}</h1>
-        <p className="text-center text-balance">{"Jeune développeur passionné par les programmes d'automatisation et le développement blockchain. Do you live and breathe code?"}</p>
+        <h1 className="text-center text-4xl font-bold text-balance">
+          {"Je transforme mes idées en programmes fonctionnels et évolutifs."}
+        </h1>
+        <p className="text-center text-balance">
+          {
+            "Jeune développeur passionné par les programmes d'automatisation et le développement blockchain. Do you live and breathe code?"
+          }
+        </p>
         <Button asChild variant="secondary">
           <Link href="mailto:andymethion@gmail.com" onClick={copyEmail}>
             <Copy />
