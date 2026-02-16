b32909d776e51e2f25a753fed05f9fc7e634bbdc
Andy Méthion <andymethion@gmail.com>
2026-02-16 16:48:35 +0100

    feat: update project details and images, add new assets

--- app/projects.tsx
+++ app/projects.tsx
@@ -10,32 +10,25 @@ type Project = {
 
 const projects = [
   {
-    image: "/twitter.jpg",
+    image: "/twitter-marketing.jpg",
     link: "https://github.com/d60/twikit",
-    name: "Twitter",
+    name: "Twitter Marketing",
     description:
-      "Mon premier projet d'envergure a consisté à développer des outils d'automatisation pour Twitter, que je vendais ensuite à l'unité à des particuliers via Discord ou Telegram. Pour cela, j'analysais les requêtes https échangées entre mon navigateur et les serveurs de Twitter, afin de les reproduire et d'imiter le comportement humain. Grâce à un simple programme Python, j'étais ainsi capable de piloter des milliers de comptes Twitter en utilisant leurs cookies de connexion.",
+      "Une suite d'outils de marketing Python automatisant les interactions Twitter via une API non officielle. La suite me permettait de booster l'engagement de comptes ou de tweets et intègre un module de web scraping pour l'extraction de données ciblées à très grande échelle.",
   },
   {
     image: "/the-shit-bot.jpg",
     link: "https://www.theshitbot.com/",
     name: "The Shit Bot",
     description:
-      "Deux ans plus tard, j'ai découvert que d'autres utilisaient des techniques similaires aux miennes, mais appliquées aux sites de sneakers afin d'acheter en masse les modèles les plus recherchés. C'est à ce moment-là que j'ai entendu parler de The Shit Bot, une référence dans ce domaine. En constatant que leur interface graphique laissait à désirer, je les ai spontanément contactés sur Twitter pour leur proposer mes compétences. Cette collaboration m'a permis de me former à Angular, qui est depuis devenu mon framework web favori",
-  },
-  {
-    image: "/blockchain.webp",
-    link: "https://github.com/andymethion/listener-rs",
-    name: "Blockchain",
-    description:
-      "Les paiements en crypto-monnaies que je recevais ont éveillé mon intérêt pour la blockchain. Cela m'a permis d'approfondir des notions telles que la cryptographie, les réseaux peer-to-peer et la décentralisation. J'ai vite compris que la blockchain constituait un écosystème innovant dont je souhaitais m'inspirer. J'ai ainsi réalisé quelques projets personnels, comme un traqueur d'adresses ou un gestionnaire d'événements.",
+      "Un partenariat avec The Shit Bot, une application de bureau d'achat automatisés de sneakers. À l'aide d'une maquette fournie, je m'occuperai de la refonte de leur interface utilisateur en utilisant Angular, un framework web que j'ai découvert dans le cadre de ce partenariat.",
   },
   {
     image: "/quadtree.png",
     link: "https://github.com/andymethion/quadtree",
     name: "Quadtree",
     description:
-      "Ce projet m'a été proposé durant ma première année d'études en informatique. Il s'agissait de concevoir un petit jeu vidéo en Go, dans lequel il fallait progressivement ajouter de nouvelles fonctionnalités. L'objectif principal était de modéliser le sol à l'aide d'un quadtree, une structure arborescente similaire à un arbre binaire, mais avec quatre enfants par nœud. Nous avons également développé d'autres fonctionnalités, telles que le zoom ou encore l'utilisation de téléporteurs.",
+      "Un jeu vidéo développé en Golang mettant l'accent sur l'optimisation algorithmique. Le projet repose sur une modélisation du terrain via un quadtree pour la gestion spatiale, intégrant des mécaniques de jeu avancées telles que le zoom dynamique et un système de téléporteurs.",
   },
   {
     image: "/qui-est-ce.png",
@@ -45,15 +38,15 @@ const projects = [
       "Une implémentation du jeu Qui-Est-Ce?, développée en Kotlin et JavaFX. Les joueurs peuvent démarrer une nouvelle partie ou rejoindre celle d'un ami. Les joueurs posent des questions à tour de rôle jusqu'à ce qu'ils soient prêts à désigner la personne concordante.",
   },
   {
-    image: "/tcp-server.png",
+    image: "/tcp-file-transfer-server.png",
     link: "https://github.com/andymethion/tcp-file-transfer-server",
-    name: "Serveur de fichier TCP",
+    name: "Serveur de transfert de fichiers TCP",
     description:
       "Un serveur TCP Golang pour des transferts de fichiers. Ses principales fonctionnalités comprennent une arborescence interactive du répertoire du serveur, des téléchargements de fichiers et une partie administrateur permettant de gérer la visibilité des fichiers/dossiers.",
   },
   {
-    image: "/lego.png",
-    link: "https://github.com/andymethion/lego",
+    image: "/lego-store.png",
+    link: "https://github.com/andymethion/lego-store",
     name: "Boutique Lego",
     description:
       "Une application CRUD full-stack utilisant PHP CodeIgniter 4 qui simule une boutique Lego. Les fonctionnalités clés comprennent la gestion des comptes utilisateurs, un panier d'achat et un panneau d'administration pour la mise à jour et la gestion dynamiques des produits.",
